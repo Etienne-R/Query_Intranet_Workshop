e5dd8da307348cf551406840dbcfa6d54868823b
Etienne Romignac <etienne.romignac@epitech.eu>
2021-01-20 16:16:57 +0400

    print module name and module code

--- main.php
+++ main.php
@@ -8,7 +8,7 @@ function    recupAutologinFile($fileName)
     return ($line);
 }
 
-function    recupWeek(&$startDate, &$endDate)
+function    recupWeek()
 {
     $startDate = date("l d m Y", strtotime("-2 day"));
     $endDate = date("l d m Y", strtotime("+3 day"));
@@ -19,18 +19,51 @@ function    getIntraJson($autolog)
 {
     $startDate = date("Y-m-d", strtotime("-2 day"));
     $endDate = date("Y-m-d", strtotime("+4 day"));
-    print($startDate . "\n");
-    print($endDate . "\n");
-    $request = $autolog . "/planning/load?format=json&start=$(" . $startDate . ")&end=$(" . $endDate . ")";
-    // print($request . "\n");
+    $request = $autolog . "/planning/load?format=json&start=" . $startDate . "&end=" . $endDate . "";
+    // $request = "{$autolog}/planning/load?format=json&start=$({$startDate})&end=$({$endDate})";
+    return ($request);
+}
+
+function    doCurlRequest($link)
+{
+    $request = curl_init();
+    curl_setopt($request, CURLOPT_URL, $link);
+    curl_setopt($request, CURLOPT_HEADER, false);
+    curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($request);
+    curl_close($request);
+    return ($response);
+}
+
+function    recupConfigFile()
+{
+    $configFile = file_get_contents('config.json');
+    $configJson = json_decode($configFile);
+    // print($configFile);
+    return ($configJson);
 }
 
 function    mainFunction()
 {
     $autolog = "";
     $autolog = recupAutologinFile("autologin.txt");
-    recupWeek($startDate, $endDate);
-    getIntraJson($autolog);
+    // recupWeek();
+    $request = getIntraJson($autolog);
+    $response = doCurlRequest($request);
+    $decodedJson = json_decode($response);
+    $configJson = recupConfigFile();
+    foreach ($decodedJson as $elem) {
+        foreach($elem as $key => $value) {
+            if ($key == $configJson->ModuleCode) {
+                print($value);
+            }
+            print(" ");
+            if ($key == $configJson->ModuleName) {
+                print($value);
+            }
+        }
+        print("\n");
+    }
 }
 
 mainFunction()
