ac82bcd29f0ea5a2a7d751761dbd323e4dd97ff2
Etienne Romignac <etienne.romignac@epitech.eu>
2021-01-20 15:26:30 +0400

    step 2 finished

--- main.php
+++ main.php
@@ -8,6 +8,30 @@ function    recupAutologinFile($fileName)
     return ($line);
 }
 
-recupAutologinFile("autologin.txt")
+function    recupWeek(&$startDate, &$endDate)
+{
+    $startDate = date("l d m Y", strtotime("-2 day"));
+    $endDate = date("l d m Y", strtotime("+3 day"));
+    print($startDate . " to ". $endDate . "\n");
+}
 
+function    getIntraJson($autolog)
+{
+    $startDate = date("Y-m-d", strtotime("-2 day"));
+    $endDate = date("Y-m-d", strtotime("+4 day"));
+    print($startDate . "\n");
+    print($endDate . "\n");
+    $request = $autolog . "/planning/load?format=json&start=$(" . $startDate . ")&end=$(" . $endDate . ")";
+    // print($request . "\n");
+}
+
+function    mainFunction()
+{
+    $autolog = "";
+    $autolog = recupAutologinFile("autologin.txt");
+    recupWeek($startDate, $endDate);
+    getIntraJson($autolog);
+}
+
+mainFunction()
 ?>
